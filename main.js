24564f7dda1bf22ad72840a540a5436c05cd3091
andylee10001 <106939389+andylee10001@users.noreply.github.com>
2024-01-30 16:24:18 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function getResultBasedOnInput(input) {
+    if (input === 'Hello') {
+        return '你好！';
+    } else if (input === 'OpenAI') {
+        return '這是一個簡單的例子，你可以根據實際需求進行修改。';
+    } else {
+        return '找不到相應的結果。';
+    }
+}
