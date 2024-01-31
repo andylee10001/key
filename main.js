724c01c98cf44a95abfdfd4baae01b5df875f0ef
andylee10001 <106939389+andylee10001@users.noreply.github.com>
2024-01-31 19:10:00 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ function getResultBasedOnInput(input) {
         return '你好！';
     } else if (input === 'OpenAI') {
         return '這是一個簡單的例子，你可以根據實際需求進行修改。';
+    } else if (input === 'offinel') {
+        return '你是個智障白癡';    
     } else {
         return '找不到相應的結果。';
     }
